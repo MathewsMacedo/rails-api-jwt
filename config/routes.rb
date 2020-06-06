2b609181110cd3e542b61b734c73fa3809a3240d
Matheus Macedo <matheus.smacedo@hotmail.com>
2020-06-06 18:48:00 -0300

    Add routes organizing by folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :restrito do
+      namespace :v1 do
+        namespace :auth do
+          post '/auth/login', to: 'authentication#login'
+        end
+      end
+    end
+  end
+    resources :users, param: :_username
+    get '/*a', to: 'application#not_found'
+
 end
