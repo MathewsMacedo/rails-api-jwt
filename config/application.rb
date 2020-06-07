88f6159bcebbae6989b08432edd005f90964dab6
Matheus Macedo <matheus.smacedo@hotmail.com>
2020-06-06 22:49:41 -0300

    Load lib/auth class

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module RailsJwt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     #load lib
-    config.autoload_paths << "#{Rails.root}/lib"
+    config.autoload_paths << "#{Rails.root}/lib/auth"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
